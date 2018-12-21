7870d1734c1cd461327928797386853dcb8be4ed
achref-benahmed <achref.benhmed@gmail.com>
2018-12-21 02:09:17 +0100

    onInputChange function

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,13 +2,18 @@ import React from 'react' ;
 
 class SearchBar extends React.Component {
 
+  onInputChange(event){
+
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className= ' ui segment'>
         <form className = 'ui form' >
           <div className = 'ui field'>
             <label > Search For an Image: </label>
-            <input type='text'  />
+            <input type='text' onChange={this.OnInputChange} />
           </div>
         </form>
       </div>
